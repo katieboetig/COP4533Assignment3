3893c326cc95e358de8e95878c1994d8a72782ac
sateipel <sateipel@gmail.com>
2026-04-04 10:25:01 -0400

    add in plotting algo and create graph of tests that were ran

--- data/plot_graph.py
+++ data/plot_graph.py
@@ -1,2 +1,13 @@
-#reads timing_results.txt and generates a runtime graph
-#run after running run_timing.py
+import matplotlib.pyplot as plt
+
+tests= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+times= [0.057, 0.022, 0.021, 0.021, 0.021, 0.021, 0.024, 0.028, 0.042, 0.029]
+
+plt.figure()
+plt.plot(tests, times, marker='o')
+
+plt.xlabel('Test Number')
+plt.ylabel('Running Time (s)')
+plt.title('Runtime Analysis')
+plt.savefig('runtime_analysis.png')
+plt.show()
